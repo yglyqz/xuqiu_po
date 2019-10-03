55554dc23a99a5b1612c000639db6012581b7bde
liyang <746597961@qq.com>
2019-10-03 11:30:30 +0800

    修改读取配置文件路径

--- utils/read_ini.py
+++ utils/read_ini.py
@@ -2,14 +2,10 @@
 
 import configparser
 
-# read_ini = configparser.ConfigParser()
-# read_ini.read("/Users/liyang/liyang/xueqiu/xueqiu_po/config/Localelement.ini")
-# print(read_ini.get("db", "db_host"))
-
 class ReadIni:
     def __init__(self, file_path=None):
         if not file_path:
-            self.file_path = "/Users/liyang/liyang/xueqiu/xueqiu_po/config/Localelement.ini"
+            self.file_path = "/Users/liyang/liyang/xueqiu_po/config/Localelement.ini"
         else:
             self.file_path = file_path
         self.data = self.read_ini()
@@ -30,4 +26,4 @@ class ReadIni:
 
 if __name__ == '__main__':
     read_ini = ReadIni()
-    print(read_ini.get_value("login_mor"))
+    print(read_ini.get_value("profile_ico"))
